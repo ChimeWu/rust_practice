eda3d39329e69a7fb201a31bc042c74ca70d2a5e
ChimeWu <wujicheng0215@outlook.com>
2024-06-01 00:58:41 +0800

    feature:cli preliminary implementation

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,3 +1,39 @@
+use clap::{Args, Parser, Subcommand};
+use reqwest::{Client, Response};
+
+#[derive(Parser, Debug)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    Get(Get),
+    Post(Post),
+}
+
+#[derive(Args, Debug)]
+pub struct Get {
+    #[arg(short, long)]
+    pub url: String,
+}
+
+#[derive(Args, Debug)]
+pub struct Post {
+    #[arg(short, long)]
+    pub url: String,
+    #[arg(short, long)]
+    pub body: String,
+}
+
+pub async fn get(arg: &Get) -> anyhow::Result<Response> {
+    let client = Client::new();
+    let response = client.get(&arg.url).send().await?;
+    Ok(response)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("{:?}", args);
 }
